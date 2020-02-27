771ce68a135ac82fb02abc4bbe6964712c25885b
Singh Kiran <singh.kiran2456@hotmail.com>
2020-02-27 17:43:57 +0100

    working loper

--- Code/Chess/Chess/board.cpp
+++ Code/Chess/Chess/board.cpp
@@ -8,20 +8,20 @@ Pawn Board::getPiece(Position position) {
    return m_board[x][y];
 }
 
-bool Board::checkRestrictions(int curr_x, int curr_y,int next_x,int next_y)
+bool Board::checkRestrictions(int curr_x, int curr_y,int next_x,int next_y,Pawn &p)
 {
 	bool res =false;
-	Pawn p = m_board[curr_x][curr_y];
+	//Pawn p = m_board[curr_x][curr_y];
 	if (p.firtsTurn())
 	{
-		if (curr_y ==next_y && (next_x == curr_x+2) )
+		if (curr_y ==next_y && (next_x == curr_x+2) || (next_x == curr_x + 1))
 		{
 			p.setfirstTurnFalse();
 			return true;
 		}
-		else if ((next_x == curr_x + 1)) {
-			return true;
-		}
+	}
+	else if ((next_x == curr_x + 1)) {
+		return true;
 	}
 	return res;
 }
@@ -34,7 +34,7 @@ void Board::move(std::string currentpos, std::string moveTo) {
     next.setpos(moveTo);
 
     Pawn piece = getPiece(current.getx(), current.gety());
-		if (checkRestrictions(current.getx(), current.gety(), next.getx(), next.gety())) {
+		if (checkRestrictions(current.getx(), current.gety(), next.getx(), next.gety(),piece)) {
 			m_board[next.getx()][next.gety()] = piece;
 			Pawn emptypawn;
 			m_board[current.getx()][current.gety()] = emptypawn;
