885f02f226c0908c31f35be8c085be29cde78122
Singh Kiran <singh.kiran2456@hotmail.com>
2020-03-03 21:37:56 +0100

    Auto stash before merge of "master" and "origin/master"

--- Code/Chess/Chess/game.cpp
+++ Code/Chess/Chess/game.cpp
@@ -47,4 +47,47 @@ void Game::nextturn() {
 
 Player* Game::currentPlayer() {
     return m_turn;
-};
+}
+void Game::run(Game newGame)
+{
+    bool checkwin;
+
+    Board newBoard{}; // Creates the board and places the pawns
+    newBoard.printBoard();
+
+    std::string curr;
+    std::string next;
+
+    do {
+        std::cout << "It's " << (newGame.currentPlayer())->name() << "'s turn" << std::endl;
+        if ((newGame.currentPlayer())->get_is_ai())
+            newBoard.AiMove(newGame.currentPlayer());
+        else {
+            do {
+                curr = "";
+                next = "";
+                do {
+                    std::cout << "Move which pawn? ";
+                    std::cin >> curr;
+                    curr[0] = toupper(curr[0]);
+                } while (checkInput(curr) == false);
+
+                do {
+                    std::cout << "To where? ";
+                    std::cin >> next;
+                    next[0] = toupper(next[0]);
+                } while (checkInput(next) == false);
+
+            } while (newBoard.move(curr, next, newGame.currentPlayer()) == false); // If a move is invalid, a turn isn't skipped
+
+        }
+
+        newBoard.printBoard();
+        if ((checkwin = newBoard.checkWin()) == false)
+            newGame.nextturn();
+
+    } while (checkwin == false); //as long as no one has won, ask the next player for their move
+
+    printWinner((newGame.currentPlayer())->name());
+}
+;
--- Code/Chess/Chess/main.cpp
+++ Code/Chess/Chess/main.cpp
@@ -1,54 +1,10 @@
 ﻿#include <iostream>
 #include "game.h"
-#include "IO.h"
 
 int main() {
 	printLogo();
-	bool gametype = checkChoice(); //VS AI || VS Player
-	bool checkwin;
-	clearScreen();
-
-	Game newGame{gametype}; // Creates both player objects, randomly assigns them their color
-
-	Board newBoard{}; // Creates the board and places the pawns
-	clearScreen();
-	newBoard.printBoard();
-
-	std::string curr;
-	std::string next;
-
-	do {
-		std::cout << "It's " << (newGame.currentPlayer())->name() << "'s turn" << std::endl;
-		if ((newGame.currentPlayer())->get_is_ai())
-			newBoard.AiMove(newGame.currentPlayer());
-		else {
-			do {
-				curr = "";
-				next = "";
-				do {
-					std::cout << "Move which pawn? ";
-					std::cin >> curr;
-					curr[0] = toupper(curr[0]);
-				} while (checkInput(curr) == false);
-
-				do {
-					std::cout << "To where? ";
-					std::cin >> next;
-					next[0] = toupper(next[0]);
-				} while (checkInput(next) == false);
-
-			} while (newBoard.move( curr , next, newGame.currentPlayer()) == false); // If a move is invalid, a turn isn't skipped
-
-		}
-
-		clearScreen();
-		newBoard.printBoard();
-		if ((checkwin = newBoard.checkWin()) == false)
-		newGame.nextturn();
-
-	} while (checkwin == false); //as long as no one has won, ask the next player for their move
-
-	clearScreen();
-	printWinner((newGame.currentPlayer())->name());
+    bool gametype = checkChoice(); //VS AI || VS Player
+    Game newGame{ gametype }; // Creates both player objects, randomly assigns them their color	newGame.run();
+    newGame.run(newGame);
 	return 1;
 };
