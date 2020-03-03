dc5eb520b800930047c1433df7b15f0dd318869f
Singh Kiran <singh.kiran2456@hotmail.com>
2020-03-03 21:57:03 +0100

    Updated Clearscreen() in main loop in Game::run()

--- Code/Chess/Chess/game.cpp
+++ Code/Chess/Chess/game.cpp
@@ -4,93 +4,95 @@ using namespace std;
 
 Game::Game(bool vsAI) {
 
-    string name1, name2;
-    cout << "Player 1's name: "  ;
-    cin >> name1;
-    m_player1.setName(name1);
-    m_player1.set_ai(false);
+	string name1, name2;
+	cout << "Player 1's name: ";
+	cin >> name1;
+	m_player1.setName(name1);
+	m_player1.set_ai(false);
 
-    if (!vsAI) {
-    cout << "Player 2's name: ";
-    cin >> name2;
-    m_player2.setName(name2);
-    m_player2.set_ai(false);
-    }
+	if (!vsAI) {
+		cout << "Player 2's name: ";
+		cin >> name2;
+		m_player2.setName(name2);
+		m_player2.set_ai(false);
+	}
 
-    else {
-        m_player2.setName("AI");
-        m_player2.set_ai(true);
-    }
+	else {
+		m_player2.setName("AI");
+		m_player2.set_ai(true);
+	}
 
-    clearScreen();
+	clearScreen();
 
-    srand((unsigned int) time(nullptr)); // Use current time for random seed
-    int coinflip = rand() % 2;
+	srand((unsigned int)time(nullptr)); // Use current time for random seed
+	int coinflip = rand() % 2;
 
-    if (coinflip == 1) {
-        m_player1.setcolor(Color::White);
-        cout << m_player1.name() << " is white, " << m_player2.name() << " is black." << endl;
-        m_player2.setcolor(Color::Black);
-        m_turn = &m_player1;
-    }
-    else {
-        m_player1.setcolor(Color::Black);
-        cout << m_player2.name() << " is white, " << m_player1.name() << " is black." << endl;
-        m_player2.setcolor(Color::White);
-        m_turn = &m_player2;
-    }
+	if (coinflip == 1) {
+		m_player1.setcolor(Color::White);
+		cout << m_player1.name() << " is white, " << m_player2.name() << " is black." << endl;
+		m_player2.setcolor(Color::Black);
+		m_turn = &m_player1;
+	}
+	else {
+		m_player1.setcolor(Color::Black);
+		cout << m_player2.name() << " is white, " << m_player1.name() << " is black." << endl;
+		m_player2.setcolor(Color::White);
+		m_turn = &m_player2;
+	}
 };
 
 void Game::nextturn() {
-    if (m_turn == &m_player1)
-        m_turn = &m_player2;
-    else
-        m_turn = &m_player1;
+	if (m_turn == &m_player1)
+		m_turn = &m_player2;
+	else
+		m_turn = &m_player1;
 };
 
 Player* Game::currentPlayer() {
-    return m_turn;
+	return m_turn;
 }
 void Game::run(Game& newGame)
 {
-    bool checkwin;
+	bool checkwin;
 
-    Board newBoard{}; // Creates the board and places the pawns
-    newBoard.printBoard();
+	Board newBoard{}; // Creates the board and places the pawns
+	newBoard.printBoard();
 
-    std::string curr;
-    std::string next;
+	std::string curr;
+	std::string next;
 
-    do {
-        std::cout << "It's " << (newGame.currentPlayer())->name() << "'s turn" << std::endl;
-        if ((newGame.currentPlayer())->get_is_ai())
-            newBoard.AiMove(newGame.currentPlayer());
-        else {
-            do {
-                curr = "";
-                next = "";
-                do {
-                    std::cout << "Move which pawn? ";
-                    std::cin >> curr;
-                    curr[0] = toupper(curr[0]);
-                } while (checkInput(curr) == false);
+	do {
+		std::cout << "It's " << (newGame.currentPlayer())->name() << "'s turn" << std::endl;
+		if ((newGame.currentPlayer())->get_is_ai()) {
+			newBoard.AiMove(newGame.currentPlayer());
 
-                do {
-                    std::cout << "To where? ";
-                    std::cin >> next;
-                    next[0] = toupper(next[0]);
-                } while (checkInput(next) == false);
+		}else {
+		do {
+			curr = "";
+			next = "";
+			do {
+				std::cout << "Move which pawn? ";
+				std::cin >> curr;
+				curr[0] = toupper(curr[0]);
+			} while (checkInput(curr) == false);
 
-            } while (newBoard.move(curr, next, newGame.currentPlayer()) == false); // If a move is invalid, a turn isn't skipped
+			do {
+				std::cout << "To where? ";
+				std::cin >> next;
+				next[0] = toupper(next[0]);
+			} while (checkInput(next) == false);
+			clearScreen();
 
-        }
+		} while (newBoard.move(curr, next, newGame.currentPlayer()) == false); // If a move is invalid, a turn isn't skipped
 
-        newBoard.printBoard();
-        if ((checkwin = newBoard.checkWin()) == false)
-            newGame.nextturn();
+		}
 
-    } while (checkwin == false); //as long as no one has won, ask the next player for their move
+		newBoard.printBoard();
+		if ((checkwin = newBoard.checkWin()) == false)
+			newGame.nextturn();
 
-    printWinner((newGame.currentPlayer())->name());
+} while (checkwin == false); //as long as no one has won, ask the next player for their move
+
+printWinner((newGame.currentPlayer())->name());
 }
 ;
