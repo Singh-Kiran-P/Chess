78dd122a8be538f8b898b4e21673c3cb0094aeab
jochem <jochem.lenaers@student.uhasselt.be>
2020-03-05 21:05:27 +0100

    Made noBlockers() a private function

--- Code/Chess/Chess/board.h
+++ Code/Chess/Chess/board.h
@@ -11,7 +11,6 @@ class Board {
         bool move(Position current, Position next, Player* player);
         bool move(std::string currentpos, std::string moveTo, Player* player);
         void AiMove(Player* player);
-        bool noBlockers(Position current, Position next) const;
 
         void printBoard() const;
         bool checkWin() const;
@@ -22,6 +21,6 @@ class Board {
     private:
         Piece* m_board[SIZE_BOARD][SIZE_BOARD];
         bool validChoice(int xpos, int ypos, Color playercolor);
+        bool noBlockers(Position current, Position next) const;
 
 };
-
