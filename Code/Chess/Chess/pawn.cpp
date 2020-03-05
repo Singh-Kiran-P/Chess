2db592e292b41a288b90f61e5f3883d150a527fc
Singh Kiran <singh.kiran2456@hotmail.com>
2020-03-05 21:59:52 +0100

    Fortmated code with Visual studio and made a printId fun

--- Code/Chess/Chess/pawn.cpp
+++ Code/Chess/Chess/pawn.cpp
@@ -1,5 +1,5 @@
 #include "pawn.h"
-
+#include "TermColor.hpp"
 int Pawn::turnCount() const {
 	return m_turnCount;
 };
@@ -10,13 +10,13 @@ void Pawn::increaseTurnCount() {
 
 bool Pawn::moveRestrictions(Piece* movingpiece, Piece* nextpiece, int x, int y) {
 	Position currPos = movingpiece->getPos();
-	int curr_x{currPos.getx()};
-	int curr_y{currPos.gety()};
+	int curr_x{ currPos.getx() };
+	int curr_y{ currPos.gety() };
 
-	int next_x{x};
-	int next_y{y};
+	int next_x{ x };
+	int next_y{ y };
 
-	int max_delta_x{1};
+	int max_delta_x{ 1 };
 	if (movingpiece->getColor() == Color::White) {
 		if (next_x >= curr_x)
 			return false;
@@ -28,19 +28,35 @@ bool Pawn::moveRestrictions(Piece* movingpiece, Piece* nextpiece, int x, int y) 
 	}
 
 	if (((Pawn*)movingpiece)->turnCount() == 0)
-	max_delta_x = 2;
+		max_delta_x = 2;
 
-    if (curr_y == next_y && abs(next_x - curr_x) <= max_delta_x) {
-        if (nextpiece == nullptr)
-            return true;
-        else
-            return false;
-    }
-    else if (abs(curr_y - next_y) == 1 && abs(next_x - curr_x) == 1 && nextpiece != nullptr) {
-        if (movingpiece->getColor() != nextpiece->getColor())
-            return true;
-        else
-        	return false;
-    }
+	if (curr_y == next_y && abs(next_x - curr_x) <= max_delta_x) {
+		if (nextpiece == nullptr)
+			return true;
+		else
+			return false;
+	}
+	else if (abs(curr_y - next_y) == 1 && abs(next_x - curr_x) == 1 && nextpiece != nullptr) {
+		if (movingpiece->getColor() != nextpiece->getColor())
+			return true;
+		else
+			return false;
+	}
 	return false;
-};
+}
+void Pawn::printId(Piece* p)
+{
+	if (p->getColor() == Color::Black)
+	{
+		std::cout << termcolor::blue;
+		std::cout << "P";
+		std::cout << termcolor::white;
+
+	}
+	else
+	{
+		std::cout << "p";
+
+	}
+}
+
