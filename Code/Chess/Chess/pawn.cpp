971a31942345c7f83253c2854b3936502264474d
jochem.lenaers <jochem.lenaers@student.uhasselt.be>
2020-03-05 11:26:37 +0100

    Squashed pawm::moverestrictions

--- Code/Chess/Chess/pawn.cpp
+++ Code/Chess/Chess/pawn.cpp
@@ -9,44 +9,31 @@ void Pawn::increaseTurnCount() {
 };
 
 bool Pawn::moveRestrictions(int curr_x, int curr_y, int next_x, int next_y, Piece* movingpiece, Piece* nextpiece) {
-
-
+	int max_delta_x;
 	if (movingpiece->getColor() == Color::White) {
-        int max_delta_x = -1;
-        if (((Pawn*)movingpiece)->turnCount() == 0)
-            max_delta_x--;
+		if (next_x >= curr_x)
+			return false;
+		max_delta_x = -1;
+	}
+	else if (movingpiece->getColor() == Color::Black) {
+		if (next_x <= curr_x)
+			return false;
+		max_delta_x = 1;
+	}
+	if (((Pawn*)movingpiece)->turnCount() == 0)
+	max_delta_x = 2 * max_delta_x;
 
-        if (curr_y == next_y && (max_delta_x <= (next_x - curr_x) && (next_x - curr_x) < 0)) {
-            if (nextpiece == nullptr)
-                return true;
-            else
-                return false;
-        }
-        else if (abs(curr_y - next_y) == 1 && (next_x - curr_x) == -1 && nextpiece != nullptr) {
-                if (movingpiece->getColor() != nextpiece->getColor())
-                    return true;
-                else
-                    return false;
-        }
+    if (curr_y == next_y && (next_x - curr_x)  <= max_delta_x) {
+        if (nextpiece == nullptr)
+            return true;
+        else
+            return false;
     }
-
-    else if (movingpiece->getColor() == Color::Black) {
-        int max_delta_x = 1;
-        if (((Pawn*)movingpiece)->turnCount() == 0)
-            max_delta_x++;
-
-            if (curr_y == next_y && (0 < (next_x - curr_x) && (next_x - curr_x) <= max_delta_x)) {
-                if (nextpiece == nullptr)
-                    return true;
-                else
-                    return false;
-            }
-        else if (abs(curr_y - next_y) == 1 && (next_x - curr_x) == 1  && nextpiece != nullptr) {
-                if (movingpiece->getColor() != nextpiece->getColor())
-                    return true;
-                else
-                    return false;
-        }
+    else if (abs(curr_y - next_y) == 1 && abs(next_x - curr_x) == 1 && nextpiece != nullptr) {
+        if (movingpiece->getColor() != nextpiece->getColor())
+            return true;
+        else
+        	return false;
     }
 	return false;
 };
