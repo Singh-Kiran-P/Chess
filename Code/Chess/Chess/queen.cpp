025cdd5f0173a980674d05c02615e47549a1298e
Singh Kiran <singh.kiran2456@hotmail.com>
2020-03-09 20:49:01 +0100

    Implemented moveRestrictions() for Queen

--- Code/Chess/Chess/queen.cpp
+++ Code/Chess/Chess/queen.cpp
@@ -2,6 +2,23 @@
 
 bool Queen::moveRestrictions(Piece* nextpiece, Position nextPos)
 {
-	return true;
+	int curr_x = m_position.getx();
+	int curr_y = m_position.gety();
+
+	int next_x = nextPos.getx();
+	int next_y = nextPos.gety();
+
+	if (nextpiece != nullptr) {
+		if (nextpiece->getColor() == m_color)
+			return false;
+	}
+
+
+	if (abs(next_x - curr_x)<8 && abs(next_y - curr_y)<8 )
+		return true;
+	else
+		return false;
+
+	
 }
 Queen::Queen(char id, Color color, Position Pos) :Piece(id, color, Pos) {};
