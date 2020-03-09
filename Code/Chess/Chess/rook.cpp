e021e9de81b88ac77ed7251fbb052fc66c928431
jochem <jochem.lenaers@student.uhasselt.be>
2020-03-09 20:28:08 +0100

    Implemented moveRestrictions() for Rook

--- Code/Chess/Chess/rook.cpp
+++ Code/Chess/Chess/rook.cpp
@@ -1,8 +1,21 @@
 #include "rook.h"
 
-bool Rook::moveRestrictions(Piece* nextpiece, Position nextPos)
-{
-	return true;
-}
+bool Rook::moveRestrictions(Piece* nextpiece, Position nextPos) {
+	int curr_x = m_position.getx();
+	int curr_y = m_position.gety();
+
+	int next_x = nextPos.getx();
+	int next_y = nextPos.gety();
+
+	if (nextpiece != nullptr) {
+		if (nextpiece->getColor() == m_color)
+			return false;
+	}
+
+	if ((curr_x == next_x) || (curr_y == next_y))
+		return true;
+	else
+		return false;
+};
 
 Rook::Rook(char id, Color color, Position Pos) :Piece(id, color, Pos) {};
