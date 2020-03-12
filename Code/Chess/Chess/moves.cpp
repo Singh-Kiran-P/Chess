e3d0d8d79e668694eaea3a14adcde722e60b5582
Singh Kiran <singh.kiran2456@hotmail.com>
2020-03-12 15:44:36 +0100

    update UML

--- Code/Chess/Chess/moves.cpp
+++ Code/Chess/Chess/moves.cpp
@@ -30,10 +30,22 @@ void Moves::addMove(char id, Color color, Position curr, Position next) {
 };
 
 void Moves::print() const {
-	for (int i = 0; i < m_moves.size(); i++) {
-		std::cout << "Id: " << m_moves.at(i).getId() << m_moves.at(i).getColor()
+	int totBlack = 0;
+	int totWhite = 0;
+	int i = 0;
+	for (i = 0; i < m_moves.size(); i++) {
+		if (m_moves.at(i).getColor() == "(Black)")
+			totBlack++;
+		else
+			totWhite++;
+		std::cout << i<<") " <<"Id: " << m_moves.at(i).getId() << m_moves.at(i).getColor()
 			<< " From: " <<  m_moves.at(i).getCurrPos().getYstr() << 8 - m_moves.at(i).getCurrPos().getx()
 			<< " To: " << m_moves.at(i).getNextPos().getYstr() << 8 - m_moves.at(i).getNextPos().getx()
 			<<endl;
 	}
+	if (m_moves.at(i-1).getColor() == "(Black)")
+		cout << endl << "Tot Moves Player Black:" << totBlack;
+	else
+		cout << endl << "Tot Moves Player White:" << totWhite;
+	cout << endl;
 };
