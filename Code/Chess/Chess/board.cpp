43c8903fb7d6fb14145cb856b92333097207dabc
jochem.lenaers <jochem.lenaers@student.uhasselt.be>
2020-03-12 13:52:02 +0100

    Fixed bug where solo king couldn't move but wasn't checkamte either

--- Code/Chess/Chess/board.cpp
+++ Code/Chess/Chess/board.cpp
@@ -40,8 +40,12 @@ bool Board::checkWin() {
 		cout << termcolor::red << "Check Black King\n" << termcolor::reset << endl;
 	}
 
-	if (CheckedKing == nullptr)
-		return false;
+	if (CheckedKing == nullptr) {
+		if (m_blackPieceCount == 1)
+			CheckedKing = BlackKing;
+		else if (m_whitePieceCount == 1)
+			CheckedKing = WhiteKing;
+	}
 
 	if (CheckedKing != nullptr || m_blackPieceCount == 1 || m_whitePieceCount == 1) {
 		int curr_x = (CheckedKing->getPos()).getx();
@@ -165,52 +169,40 @@ Board::Board() {
 	Color PieceColor{};
 	Position tempPos{};
 	Piece* p_ptr{};
-	tempPos.setpos("D4");
-	m_board[4][3] = new King('K', Color::Black, tempPos);
-	tempPos.setpos("C8");
-	m_board[0][2] = new Rook('R', Color::White, tempPos);
-	tempPos.setpos("E8");
-	m_board[0][4] = new Rook('R', Color::White, tempPos);
-	tempPos.setpos("A5");
-	m_board[3][0] = new Rook('R', Color::White, tempPos);
-	tempPos.setpos("A1");
-	m_board[7][0] = new Rook('R', Color::White, tempPos);
-	tempPos.setpos("H1");
-	m_board[7][7] = new King('K', Color::White, tempPos);
-	//for (int i = 0; i < SIZE_BOARD; i++) {
-	//	for (int j = 0; j < SIZE_BOARD; j++) {
 
-	//		if (i <= 1) {
-	//			tempPos.setpos(i, j);
-	//			PieceColor = Color::Black; // top of board is black
-	//		}
-	//		else if (i >= 6) {
-	//			tempPos.setpos(i, j);
-	//			PieceColor = Color::White; // bottom of board is white
-	//		}
+	for (int i = 0; i < SIZE_BOARD; i++) {
+		for (int j = 0; j < SIZE_BOARD; j++) {
+					if (i <= 1) {
+				tempPos.setpos(i, j);
+				PieceColor = Color::Black; // top of board is black
+			}
+			else if (i >= 6) {
+				tempPos.setpos(i, j);
+				PieceColor = Color::White; // bottom of board is white
+			}
 
-	//		if (i == 1 || i == 6) {
-	//			p_ptr = new Pawn{ 'P', PieceColor, tempPos };
-	//			m_board[i][j] = p_ptr;
-	//		}
-	//		else if (i == 0 || i == 7) {
+			if (i == 1 || i == 6) {
+				p_ptr = new Pawn{ 'P', PieceColor, tempPos };
+				m_board[i][j] = p_ptr;
+			}
+			else if (i == 0 || i == 7) {
 
-	//			if (j == 0 || j == 7) // Rooks
-	//				p_ptr = new Rook{ 'R', PieceColor, tempPos };
-	//			else if (j == 1 || j == 6) //Knight
-	//				p_ptr = new Knight{ 'N', PieceColor, tempPos };
-	//			else if (j == 2 || j == 5) //Bishop
-	//				p_ptr = new Bishop{ 'B', PieceColor, tempPos };
-	//			else if (j == 3) //Queen
-	//				p_ptr = new Queen{ 'Q', PieceColor, tempPos };
-	//			else if (j == 4) //King
-	//				p_ptr = new King{ 'K', PieceColor, tempPos };
-	//			m_board[i][j] = p_ptr;
-	//		}
-	//		else
-	//			m_board[i][j] = nullptr;
-	//	}
-	//}
+				if (j == 0 || j == 7) // Rooks
+					p_ptr = new Rook{ 'R', PieceColor, tempPos };
+				else if (j == 1 || j == 6) //Knight
+					p_ptr = new Knight{ 'N', PieceColor, tempPos };
+				else if (j == 2 || j == 5) //Bishop
+					p_ptr = new Bishop{ 'B', PieceColor, tempPos };
+				else if (j == 3) //Queen
+					p_ptr = new Queen{ 'Q', PieceColor, tempPos };
+				else if (j == 4) //King
+					p_ptr = new King{ 'K', PieceColor, tempPos };
+				m_board[i][j] = p_ptr;
+			}
+			else
+				m_board[i][j] = nullptr;
+		}
+	}
 };
 
 Board::~Board() {
