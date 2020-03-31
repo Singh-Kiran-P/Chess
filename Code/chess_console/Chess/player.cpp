55b201bdf7ba878709db6c76783b6957716b3954
Singh Kiran <singh.kiran2456@hotmail.com>
2020-03-31 10:36:56 +0200

    folder names update

---
+++ Code/chess_console/Chess/player.cpp
@@ -0,0 +1,11 @@
+#include "player.h"
+
+Color Player::color() const{
+    return m_color;
+};
+
+std::string Player::name()const {
+    return m_playername;
+};
+
+Player::Player(string nameStr, Color color) : m_playername{ nameStr }, m_color{ color } {};
