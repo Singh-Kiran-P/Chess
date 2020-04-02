86aee901219cd618e59795de4dd02de9081ba723
Singh Kiran <singh.kiran2456@hotmail.com>
2020-04-02 11:25:41 +0200

    added de Piece class to boardView

--- Code/chess_Qt/Chess/pieceview.cpp
+++ Code/chess_Qt/Chess/pieceview.cpp
@@ -1,13 +1,16 @@
 #include "pieceview.h"
-
-PieceView::PieceView(QString path) {
+#include <QDebug>
+PieceView::PieceView(QString path,Piece* piece) {
 
     QPixmap pawn(path);
     setPixmap(pawn);
     setFlag(QGraphicsItem::ItemIsSelectable);
+    m_Piece = piece;
 }
 
 void PieceView::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent) {
         if(mouseEvent->modifiers() & Qt::ControlModifier)
             mouseEvent->ignore();
+         qDebug() <<"x="<< m_Piece->getPos().getx()<<"y="<<m_Piece->getPos().gety()<<endl;
+
 }
